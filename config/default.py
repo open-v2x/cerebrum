d8c9051a96e1b9a5c53e3378a2add23211a270d2
gaotianyu <gao.tianyu@99cloud.net>
2022-07-18 16:45:22 +0800

    feat: change config default.py without default variable setup
    issue: 140234
    Change-Id: I6b587b2b61c0cd98a20a24dc15a03d369812600e

--- config/default.py
+++ config/default.py
@@ -1,17 +1,19 @@
 import os
 import uuid
 
-
 DEFAULT_HOST_MQTT = "139.196.13.9"
+DEFAULT_PASSOWRD_MQTT = "abc@1234" # user define
 DEFAULT_HOST_REDIS = "139.196.13.9"
+DEFAULT_PASSWORD_REDIS = "redis12345" # user define
 DEFAULT_HOST_MYSQL = "139.196.13.9"
+DEFAULT_PASSOWRD_MYSQL = "abc@1234" # user define
 DEFAULT_CLOUD_URL = "http://139.196.13.9:28300/api/v1"
 DELIMITER = "/"
 
 redis = {
     "host": os.getenv("redis_host") or DEFAULT_HOST_REDIS,
     "port": 6379,
-    "password": "redis12345",
+    "password": os.getenv("redis_password") or DEFAULT_PASSWORD_REDIS,
     "db": 1,
 }
 
@@ -20,14 +22,14 @@ mqtt = {
     "port": 1883,
     "client_id": uuid.uuid4().hex,
     "username": "root",
-    "password": "abc@1234",
+    "password": os.getenv("emqx_password") or DEFAULT_PASSOWRD_MQTT,
 }
 
 mysql = {
-    "host": DEFAULT_HOST_MYSQL,
+    "host": os.getenv("mysql_host") or DEFAULT_HOST_MYSQL,
     "port": 3306,
     "user": "root",
-    "password": "abc@1234",
+    "password": os.getenv("mysql_root_password") or DEFAULT_PASSOWRD_MYSQL,
     "charset": "utf8",
     "db": "dandelion",
 }
