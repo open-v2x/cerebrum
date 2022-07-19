82960f1f1f4b8f6729c14ec115b082343dd5d9d3
Wu Wenxiang <wu.wenxiang@99cloud.net>
2022-07-19 15:17:10 +0800

    add github gate & ci actions
    1. add github gate & ci actions
    2. fix default const key misspelling
    3. not use mysql root user
    
    issue: 141898
    Change-Id: I3734437847b34ecda6b263b529d9028b0c2aa9cb

--- config/default.py
+++ config/default.py
@@ -1,35 +1,37 @@
 import os
 import uuid
 
-DEFAULT_HOST_MQTT = "139.196.13.9"
-DEFAULT_PASSOWRD_MQTT = "v2x2022" # user define
-DEFAULT_HOST_REDIS = "139.196.13.9"
-DEFAULT_PASSWORD_REDIS = "v2x2022" # user define
-DEFAULT_HOST_MYSQL = "139.196.13.9"
-DEFAULT_PASSOWRD_MYSQL = "v2x2022" # user define
+
+DEFAULT_MQTT_HOST = "139.196.13.9"
+DEFAULT_MQTT_PASSWORD = "v2x2022"  # user define
+DEFAULT_REDIS_HOST = "139.196.13.9"
+DEFAULT_REDIS_PASSWORD = "v2x2022"  # user define
+DEFAULT_MYSQL_HOST = "139.196.13.9"
+DEFAULT_MYSQL_USER = "dandelion"
+DEFAULT_MYSQL_PASSWORD = "v2x2022"  # user define
 DEFAULT_CLOUD_URL = "http://139.196.13.9:28300/api/v1"
 DELIMITER = "/"
 
 redis = {
-    "host": os.getenv("redis_host") or DEFAULT_HOST_REDIS,
+    "host": os.getenv("redis_host") or DEFAULT_REDIS_HOST,
     "port": 6379,
-    "password": os.getenv("redis_password") or DEFAULT_PASSWORD_REDIS,
+    "password": os.getenv("redis_password") or DEFAULT_REDIS_PASSWORD,
     "db": 1,
 }
 
 mqtt = {
-    "host": os.getenv("mqtt_host") or DEFAULT_HOST_MQTT,
+    "host": os.getenv("mqtt_host") or DEFAULT_MQTT_HOST,
     "port": 1883,
     "client_id": uuid.uuid4().hex,
     "username": "root",
-    "password": os.getenv("emqx_password") or DEFAULT_PASSOWRD_MQTT,
+    "password": os.getenv("emqx_password") or DEFAULT_MQTT_PASSWORD,
 }
 
 mysql = {
-    "host": os.getenv("mysql_host") or DEFAULT_HOST_MYSQL,
+    "host": os.getenv("mysql_host") or DEFAULT_MYSQL_HOST,
     "port": 3306,
-    "user": "root",
-    "password": os.getenv("mysql_root_password") or DEFAULT_PASSOWRD_MYSQL,
+    "user": os.getenv("mysql_user") or DEFAULT_MYSQL_USER,
+    "password": os.getenv("mysql_password") or DEFAULT_MYSQL_PASSWORD,
     "charset": "utf8",
     "db": "dandelion",
 }
