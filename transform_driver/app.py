d8fd5c5a22975ffd5f5429c2824173b33d06e82c
Wd77Baby <z.y.z.q.777@gmail.com>
2022-08-20 11:46:25 +0800

    fix: fix algorithm service repeated error
    closes: open-v2x/cerebrum#50

--- transform_driver/app.py
+++ transform_driver/app.py
@@ -61,6 +61,9 @@ class App:
             consts.topic_replace(
                 "V2X/RSU/REG/TICE", self.config.DELIMITER
             ): self._mqtt_on_db,
+            consts.topic_replace(
+                "V2X/CONFIG/UPDATE/NOTICE", self.config.DELIMITER
+            ): self._mqtt_on_config_db,
         }
         self.rsm_topic_driver_re = re.compile(
             consts.topic_replace(
@@ -128,21 +131,9 @@ class App:
         self.mqtt.on_socket_unregister_write = (
             self._mqtt_on_socket_unregister_write
         )
-
         self.mqtt.username_pw_set(mcfg["username"], mcfg["password"])
         self.mqtt.connect(mcfg["host"], mcfg["port"])
-        try:
-            mcfg_conn = db.get_mqtt_config()
-            self.mqtt_conn = mqtt.Client(client_id=uuid.uuid4().hex)
-            self.mqtt_conn.username_pw_set(
-                mcfg_conn["username"], mcfg_conn["password"]
-            )
-            self.mqtt_conn.connect(mcfg_conn["host"], mcfg_conn["port"])
-            self.process = DataProcessing(self.mqtt, self.kv, self.mqtt_conn)
-            self.svc = Service(self.mqtt, self.kv, self.mqtt_conn)
-        except Exception:
-            self.process = DataProcessing(self.mqtt, self.kv)
-            self.svc = Service(self.mqtt, self.kv)
+        self._mqtt_cfg_db()
         self.rsi = RSI(self.mqtt, self.kv)
         self.cfg = Cfg(self.kv)
 
@@ -151,6 +142,23 @@ class App:
                 getattr(signal, sig), self._handle_sigstop
             )
         return True
+
+    def _mqtt_cfg_db(self):
+        try:
+            mcfg_conn, node_id = db.get_mqtt_config()
+            self.mqtt_conn = mqtt.Client(client_id=uuid.uuid4().hex)
+            self.mqtt_conn.username_pw_set(
+                mcfg_conn["username"], mcfg_conn["password"]
+            )
+            self.mqtt_conn.connect(mcfg_conn["host"], mcfg_conn["port"])
+            self.mqtt_conn.loop_start()
+            self.process = DataProcessing(
+                self.mqtt, self.kv, self.mqtt_conn, node_id
+            )
+            self.svc = Service(self.mqtt, self.kv, self.mqtt_conn, node_id)
+        except Exception:
+            self.process = DataProcessing(self.mqtt, self.kv)
+            self.svc = Service(self.mqtt, self.kv)
 
     async def _stop(self):
         await self.kv.redis.close()
@@ -199,9 +207,12 @@ class App:
             client.message_callback_add(topic, cb)
 
     def _mqtt_on_rsm_msg(self, client, userdata, msg):
-        driver_name, rsu_id = self._driver_name(msg.topic, "rsm")
-        driver = getattr(drivers, driver_name)
-        miss_flag, rsm, miss_info = driver(msg.payload)
+        try:
+            driver_name, rsu_id = self._driver_name(msg.topic, "rsm")
+            driver = getattr(drivers, driver_name)
+            miss_flag, rsm, miss_info = driver(msg.payload)
+        except Exception:
+            return logger.error("rsm data format error")
         if self._is_valid_rsu_id(rsu_id):
             self.loop.create_task(
                 self.process.run(rsu_id, rsm, miss_flag, miss_info)
@@ -210,25 +221,34 @@ class App:
             logger.error("RSU is not registered")
 
     def _mqtt_on_vir(self, client, userdata, msg):
-        m = self.vir_topic_re.search(msg.topic)
-        rsu_id = m.groupdict()["rsuid"]
+        try:
+            m = self.vir_topic_re.search(msg.topic)
+            rsu_id = m.groupdict()["rsuid"]
+        except Exception:
+            return logger.error("vir data format error")
         if self._is_valid_rsu_id(rsu_id):
             self.loop.create_task(self.svc.run(rsu_id, msg.payload))
         else:
             logger.error("Target RSU is not registered")
 
     def _mqtt_on_rsi(self, client, userdata, msg):
-        driver_name, rsu_id = self._driver_name(msg.topic, "rsi")
-        driver = getattr(drivers, driver_name)
-        rsi, congestion_info = driver(msg.payload)
+        try:
+            driver_name, rsu_id = self._driver_name(msg.topic, "rsi")
+            driver = getattr(drivers, driver_name)
+            rsi, congestion_info = driver(msg.payload)
+        except Exception:
+            return logger.error("rsi data format error")
         if self._is_valid_rsu_id(rsu_id):
             self.loop.create_task(self.rsi.run(rsu_id, rsi, congestion_info))
         else:
             logger.error("RSU is not registered")
 
     def _mqtt_on_cfg(self, client, userdata, msg):
-        m = self.cfg_topic_re.search(msg.topic)
-        rsu_id = m.groupdict()["rsuid"]
+        try:
+            m = self.cfg_topic_re.search(msg.topic)
+            rsu_id = m.groupdict()["rsuid"]
+        except Exception:
+            return logger.error("cfg data format error")
         if self._is_valid_rsu_id(rsu_id):
             self.loop.create_task(self.cfg.run(rsu_id, msg.payload))
         else:
@@ -236,6 +256,9 @@ class App:
 
     def _mqtt_on_db(self, client, userdata, msg):
         db.get_rsu_info(msg.payload)
+
+    def _mqtt_on_config_db(self, client, userdata, msg):
+        self._mqtt_cfg_db()
 
     def _driver_name(self, topic, msg_type):
         if topic[-2:] == "UP":
